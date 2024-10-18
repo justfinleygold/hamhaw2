c7e2dfa4c7365dc420523c2028dfd6cc69806a9d
Fin Gold <justfinleygold@gmail.com>
2024-10-18 14:07:32 -0400

    navbar again

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { supabase } from '../supabaseClient';
 import HamhawShield from '../assets/hamhaw_shield.png'; // Import the shield image
-import PropTypes from 'prop-types'; // Import PropTypes
-import './navbar.css';
+import './navbar.css'; // Keep the original CSS
 
-const Navbar = ({ rootClassName, imageSrc2, imageAlt2 }) => {
+const Navbar = (props) => {
   const [user, setUser] = useState(null); // State to store logged-in user
   const navigate = useNavigate();
 
@@ -36,16 +36,26 @@ const Navbar = ({ rootClassName, imageSrc2, imageAlt2 }) => {
   };
 
   return (
-    <div className={`navbar-container ${rootClassName}`}>
+    <div className={`navbar-navbar ${props.rootClassName}`}>
       <div className="navbar-left-side">
         <img
           src={HamhawShield} // Use imported shield image
-          alt={imageAlt2}
-          className="navbar-ham-haw-shield"
+          alt="HamHaw Shield"
+          className="navbar-image1" // Use the original class from navbar.css
         />
-        <Link to="/" className="navbar-link">Home</Link>
-        <button onClick={handleBackClick} className="navbar-button">Back</button>
-        <Link to="/find" className="navbar-link">Find</Link>
+        <div data-role="BurgerMenu" className="navbar-burger-menu">
+          <svg viewBox="0 0 1024 1024" className="navbar-icon1">
+            <path d="M128 256h768v86h-768v-86zM128 554v-84h768v84h-768zM128 768v-86h768v86h-768z"></path>
+          </svg>
+        </div>
+        <div className="navbar-links-container1">
+          <Link to="/" className="Anchor">
+            <span className="navbar-text3 Anchor">Home</span>
+          </Link>
+          <button onClick={handleBackClick} className="Anchor navbar-back-button">
+            <span className="navbar-text6 Anchor">Back</span>
+          </button>
+        </div>
       </div>
       <div className="navbar-right-side">
         {user ? (
@@ -60,22 +70,50 @@ const Navbar = ({ rootClassName, imageSrc2, imageAlt2 }) => {
           </>
         )}
       </div>
+      <div data-role="MobileMenu" className="navbar-mobile-menu">
+        <div className="navbar-container">
+          <img
+            alt={props.imageAlt1}
+            src={props.imageSrc1}
+            className="navbar-image2"
+          />
+          <div data-role="CloseMobileMenu" className="navbar-close-menu">
+            <svg viewBox="0 0 1024 1024" className="navbar-icon3">
+              <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-// Define the expected PropTypes for type-checking
-Navbar.propTypes = {
-  rootClassName: PropTypes.string,  // Class name for styling
-  imageSrc2: PropTypes.string,      // Source for the image
-  imageAlt2: PropTypes.string       // Alt text for the image
+Navbar.defaultProps = {
+  link2: undefined,
+  link4: undefined,
+  link: undefined,
+  link3: undefined,
+  link5: undefined,
+  imageSrc1: 'https://play.teleporthq.io/static/svg/default-img.svg',
+  link1: undefined,
+  rootClassName: '',
+  imageAlt1: 'image',
+  imageSrc2: HamhawShield,
+  imageAlt2: 'HamHaw Shield',
 };
 
-// Default prop values in case they are not passed
-Navbar.defaultProps = {
-  rootClassName: '',
-  imageSrc2: HamhawShield,
-  imageAlt2: 'HamHaw Shield'
+Navbar.propTypes = {
+  link2: PropTypes.element,
+  link4: PropTypes.element,
+  link: PropTypes.element,
+  link3: PropTypes.element,
+  link5: PropTypes.element,
+  imageSrc1: PropTypes.string,
+  link1: PropTypes.element,
+  rootClassName: PropTypes.string,
+  imageAlt1: PropTypes.string,
+  imageSrc2: PropTypes.string,
+  imageAlt2: PropTypes.string,
 };
 
 export default Navbar;
